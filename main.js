f64ded3e7f1b3064f5010a9566115a1dc5980a6b
Igor Tsuker <igorek888rane@gmail.com>
2023-02-26 21:05:30 +0300

    init

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import './styles/all.scss'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <header>
+    <div class="header__content content">
+        <div class="header__logo">
+        
+        </div>
+        <nav class="header__navigation">
+        
+        </nav>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+  </header>
 `
 
