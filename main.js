61c7b951a045814eb207e936d219ca080994a9ce
igor <igorek888rane@gmail.com>
2023-02-27 09:07:57 +0300

    header

--- main.js
+++ main.js
@@ -4,11 +4,20 @@ document.querySelector('#app').innerHTML = `
   <header>
     <div class="header__content content">
         <div class="header__logo">
-        
+        <span class="header__name"> Elite Fire </span>
+        <p class="header__sub_name"> Автоматические биокамины</p>
         </div>
-        <nav class="header__navigation">
-        
-        </nav>
+        <div class="header__navigation">
+       <div class="header__anchor">
+        <a href="">Продукция</a>
+        <a href="">Конструктор</a>
+        <a href="">Опыт</a>
+        <a href="">Контакты</a>
+        </div>
+        <div class="header__phone">
+        <img src="./public/header/phone.png" alt="">
+        </div>
+        </div>
     </div>
   </header>
 `
