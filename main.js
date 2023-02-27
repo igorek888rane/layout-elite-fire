e87ce65f85940f6e3e0565e15d6d870c8d3b6e5b
igor <igorek888rane@gmail.com>
2023-02-27 09:27:15 +0300

    header

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
 import './styles/all.scss'
+import Header from "./component/Header/Header.js";
 
 document.querySelector('#app').innerHTML = `
-  <header>
-    <div class="header__content content">
-        <div class="header__logo">
-        <span class="header__name"> Elite Fire </span>
-        <p class="header__sub_name"> Автоматические биокамины</p>
-        </div>
-        <div class="header__navigation">
-       <div class="header__anchor">
-        <a href="">Продукция</a>
-        <a href="">Конструктор</a>
-        <a href="">Опыт</a>
-        <a href="">Контакты</a>
-        </div>
-        <div class="header__phone">
-        <img src="./public/header/phone.png" alt="">
-        </div>
-        </div>
-    </div>
-  </header>
+${Header}
 `
 
