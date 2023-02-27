ba52c931458aabe98a4aaad6d86d1b8505c57d9b
igor <igorek888rane@gmail.com>
2023-02-27 13:45:38 +0300

    refueling

---
+++ component/Refueling/Refueling.js
@@ -0,0 +1,10 @@
+const Refueling = document.createElement('section')
+Refueling.classList.add('refueling')
+Refueling.innerHTML = `
+<div class="refueling__content content">
+ 
+</div>
+
+`
+
+export default Refueling.outerHTML
--- main.js
+++ main.js
@@ -2,10 +2,12 @@ import './styles/all.scss'
 import Header from "./component/Header/Header.js";
 import Automatic from "./component/Automatic/Automatic.js";
 import RealFire from "./component/RealFire/RealFire.js";
+import Refueling from "./component/Refueling/Refueling.js";
 
 document.querySelector('#app').innerHTML = `
 ${Header}
 ${Automatic}
 ${RealFire}
+${Refueling}
 `
 
